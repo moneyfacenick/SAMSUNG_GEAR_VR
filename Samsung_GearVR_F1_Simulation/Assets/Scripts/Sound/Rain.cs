7cc1efe16aff55a577f730f3577eeed31bb3c3c0
moneyfacneick <nlyh_97@hotmail.com>
2016-04-18 09:29:28 +0800

    Added rain sound

---
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/Sound/Rain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rain : MonoBehaviour {
+
+    public AudioClip rain;
+
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+        source.playOnAwake = true;
+	}
+}
