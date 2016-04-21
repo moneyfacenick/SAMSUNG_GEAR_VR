3a04bfca6a93ad6ad231b35c30b70a5aa415dea0
unknown <143802S@M625GC01.sidm.student.local>
2016-04-21 14:49:44 +0800

    Added comments to various scripts as well as completing the integration of the gearbox and the speedo Ui display

--- Samsung_GearVR_F1_Simulation/Assets/Scripts/Waypoints.cs
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/Waypoints.cs
@@ -8,33 +8,43 @@ public class Waypoints : MonoBehaviour {
 
 	public int waypointIndex;
 
+	public float currentSpeed = 0.0f;
+
 	List<Transform> waypoints;
 
-	Vector3 Direction;
+	Vector3 direction;
 
-	public Quaternion ObjectRotation;
+	Quaternion objectRotation;
+
+	Rigidbody body;
 
 	float rotationDamping = 6.0f;
 	float accelaration = 88.0f;
 	float speedLimit = 1774.4f;
-	public float currentSpeed = 0.0f;
 
-	public bool updateWaypoint = true;
+	bool updateWaypoint = true;
 
 	// Use this for initialization
 	void Start () {
 		waypointIndex = 0;
 		GetWayPoint ();
+
+		body = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Move ();
+	}
+
+	void FixedUpdate() {
+		// Rotate the camera to face the direction of the next waypoint
 		UpdateRotate ();
-		Move ();
 	}
 
 	public void OnTriggerEnter(Collider other)
 	{
+		// If collided with a waypoint then switch to the next waypoint
 		if (other.CompareTag("Waypoint")) {
 			ChangeNextWaypoint ();
 		}
@@ -42,19 +52,24 @@ public class Waypoints : MonoBehaviour {
 		
 	void UpdateRotate()
 	{
+		// If there is a waypoint
 		if (waypoint) {
 
+			// If the waypoint have been updated 
 			if (updateWaypoint) {
-				transform.rotation = Quaternion.Lerp (transform.rotation, ObjectRotation, Time.deltaTime * rotationDamping);
 
-				if (Quaternion.Angle (transform.rotation, ObjectRotation) < 2.5f) {
-					transform.rotation = ObjectRotation;
+				// Rotate the camera to view the next waypoint using interpolation
+				body.MoveRotation (Quaternion.Lerp (transform.rotation, objectRotation, Time.deltaTime * rotationDamping));
+
+				// If camera view is looking at the next waypoint stop updating
+				if (Quaternion.Angle (transform.rotation, objectRotation) < 2.5f) {
 					updateWaypoint = false;
 				}
 			}
 		}
 	}
 
+	// Old
 	void Move()
 	{
 		currentSpeed = currentSpeed + accelaration * accelaration;
@@ -72,18 +87,24 @@ public class Waypoints : MonoBehaviour {
 
 	void ChangeNextWaypoint()
 	{
+		// Move to next waypoint
 		waypointIndex++;
 
+		// If have reach the last waypoint go back to the beginning
 		if (waypointIndex >= waypoints.Count) {
 			waypointIndex = 0;
 		}
 
+		// Update the waypoint to the next one
 		waypoint = waypoints [waypointIndex];
 
-		Direction = waypoint.position - transform.position;
-		Direction.Normalize();
+		// Calculate the direction of this waypoint to the next one
+		direction = waypoint.position - transform.position;
+		direction.y = 0;
+		direction.Normalize();
 
-		ObjectRotation = Quaternion.LookRotation (Direction);
+		// Calculate the Quaternion using the direction above
+		objectRotation = Quaternion.LookRotation (direction);
 
 		updateWaypoint = true;
 	}
@@ -93,15 +114,36 @@ public class Waypoints : MonoBehaviour {
 		Transform getwaypoints = waypoint.GetComponentInChildren<Transform>();
 		waypoints = new List<Transform>();
 
+		// Initing the waypoints into the list
 		foreach (Transform findwaypoints in getwaypoints)
 		{
 			waypoints.Add(findwaypoints);
 		}
 
+		// Update the waypoint
 		waypoint = waypoints [waypointIndex];
-		Direction = waypoint.position - transform.position;
-		Direction.Normalize();
 
-		ObjectRotation = Quaternion.LookRotation (Direction);
+		// Calculate the direction of this waypoint to the next one
+		direction = waypoint.position - transform.position;
+		direction.y = 0;
+		direction.Normalize();
+
+		// Calculate the Quaternion using the direction above
+		objectRotation = Quaternion.LookRotation (direction);
+	}
+
+	public bool GetUpdateWaypoint()
+	{
+		return updateWaypoint;
+	}
+
+	public Vector3 GetDirection()
+	{
+		return direction;
+	}
+
+	public Quaternion GetObjectRotation()
+	{
+		return objectRotation;
 	}
 }
