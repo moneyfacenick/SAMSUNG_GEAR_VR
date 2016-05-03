79a153ca1537a986117314b98f7f80754793d37f
unknown <143802S@M625GC01.sidm.student.local>
2016-05-03 15:07:48 +0800

    Rain and fov effect finish

---
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/CameraFovEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFovEffect : MonoBehaviour {
+
+	public float MaxFovAmount;
+	public float TriggerSpeed;
+	public float MaxSpeed;
+
+	public GameObject Vehicle;
+
+	Camera camera;
+
+	Rigidbody body;
+
+	float initialFOV;
+
+	// Use this for initialization
+	void Start () {
+		camera = this.GetComponent<Camera> ();
+
+		body = Vehicle.GetComponent<Rigidbody> ();
+
+		initialFOV = camera.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float currentSpeed = body.velocity.magnitude;
+
+		if (currentSpeed > TriggerSpeed && camera.fieldOfView < initialFOV + MaxFovAmount) {
+			float speed = body.velocity.magnitude - TriggerSpeed;
+
+			camera.fieldOfView = initialFOV + ((speed / (MaxSpeed - TriggerSpeed)) * MaxFovAmount);
+
+		} else if(currentSpeed < TriggerSpeed && camera.fieldOfView != initialFOV){
+			camera.fieldOfView = initialFOV;
+		}
+	}
+}
---
+++ Samsung_GearVR_F1_Simulation/Assets/Scripts/RainController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainController : MonoBehaviour {
+
+	public GameObject Rain;
+	public GameObject RainEffect;
+	public GameObject SpitEffect;
+	public int MaxSmallRain;
+	public int MaxSmallSpit;
+
+	// Use this for initialization
+	void Start () {
+
+		if (RainEffect.GetComponent<EllipsoidParticleEmitter> () == null || SpitEffect.GetComponent<EllipsoidParticleEmitter> () == null) {
+			Debug.Log ("Component not inited");
+		}
+
+		// Randomming if theres rain in the scene
+		if (Random.Range (0, 2) == 1) {
+			// Randomming between small and heavy rain
+			if (Random.Range (0, 2) == 0) {
+				// Set Rain to small
+				RainEffect.GetComponent<EllipsoidParticleEmitter>().maxEmission = MaxSmallRain;
+				SpitEffect.GetComponent<EllipsoidParticleEmitter> ().maxEmission = MaxSmallSpit;
+			}
+		} else {
+			// Turn off rain
+			Rain.SetActive (false);
+		}
+	}
+}
